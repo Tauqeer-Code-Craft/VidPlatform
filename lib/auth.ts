289a27880abc803bd46059e32c2c36872a776845
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2025-09-03 23:56:40 +0530

    initailization of auth

---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import { CredentialsProvider } from "next-auth/providers/credentials"
+
+export const authOptions : NextAuthOptions = {
+  providers:[
+    CredentialsProvider({
+
+        name: "Credentials",
+        credentials: { 
+            email: {label:"Email",type: "text"},
+            password: {label: "Password",type: "password"}
+
+        },
+        
+    })
+  ]
+}
+
+//1.26.10
